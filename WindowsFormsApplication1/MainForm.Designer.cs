238a15050983721f9ad55edecbceefe37846f143
steveschmidt1712@yahoo.com <steveschmidt1712@yahoo.com>
2016-04-09 12:37:40 -0500

    v 1.0

---
+++ WindowsFormsApplication1/MainForm.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.reservationsBtn = new System.Windows.Forms.Button();
+            this.roomManagementBtn = new System.Windows.Forms.Button();
+            this.billingBtn = new System.Windows.Forms.Button();
+            this.clientsBtn = new System.Windows.Forms.Button();
+            this.employeeSectionBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.dropBtn1 = new System.Windows.Forms.Button();
+            this.dropBtn2 = new System.Windows.Forms.Button();
+            this.dropBtn3 = new System.Windows.Forms.Button();
+            this.dropBtn4 = new System.Windows.Forms.Button();
+            this.dropBtn5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // reservationsBtn
+            // 
+            this.reservationsBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reservationsBtn.Image = global::WindowsFormsApplication1.Properties.Resources.reservations_icon_300x241;
+            this.reservationsBtn.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.reservationsBtn.Location = new System.Drawing.Point(16, 15);
+            this.reservationsBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.reservationsBtn.Name = "reservationsBtn";
+            this.reservationsBtn.Size = new System.Drawing.Size(213, 86);
+            this.reservationsBtn.TabIndex = 0;
+            this.reservationsBtn.UseVisualStyleBackColor = true;
+            this.reservationsBtn.Click += new System.EventHandler(this.reservationsBtn_Click);
+            // 
+            // roomManagementBtn
+            // 
+            this.roomManagementBtn.Location = new System.Drawing.Point(237, 15);
+            this.roomManagementBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.roomManagementBtn.Name = "roomManagementBtn";
+            this.roomManagementBtn.Size = new System.Drawing.Size(213, 86);
+            this.roomManagementBtn.TabIndex = 1;
+            this.roomManagementBtn.Text = "Room Management";
+            this.roomManagementBtn.UseVisualStyleBackColor = true;
+            this.roomManagementBtn.Click += new System.EventHandler(this.roomManagementBtn_Click);
+            // 
+            // billingBtn
+            // 
+            this.billingBtn.Location = new System.Drawing.Point(459, 15);
+            this.billingBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.billingBtn.Name = "billingBtn";
+            this.billingBtn.Size = new System.Drawing.Size(213, 86);
+            this.billingBtn.TabIndex = 2;
+            this.billingBtn.Text = "Billing";
+            this.billingBtn.UseVisualStyleBackColor = true;
+            this.billingBtn.Click += new System.EventHandler(this.billingBtn_Click);
+            // 
+            // clientsBtn
+            // 
+            this.clientsBtn.Location = new System.Drawing.Point(680, 15);
+            this.clientsBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.clientsBtn.Name = "clientsBtn";
+            this.clientsBtn.Size = new System.Drawing.Size(213, 86);
+            this.clientsBtn.TabIndex = 3;
+            this.clientsBtn.Text = "Clients";
+            this.clientsBtn.UseVisualStyleBackColor = true;
+            this.clientsBtn.Click += new System.EventHandler(this.clientsBtn_Click);
+            // 
+            // employeeSectionBtn
+            // 
+            this.employeeSectionBtn.Location = new System.Drawing.Point(901, 15);
+            this.employeeSectionBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.employeeSectionBtn.Name = "employeeSectionBtn";
+            this.employeeSectionBtn.Size = new System.Drawing.Size(213, 86);
+            this.employeeSectionBtn.TabIndex = 4;
+            this.employeeSectionBtn.Text = "Employee Section";
+            this.employeeSectionBtn.UseVisualStyleBackColor = true;
+            this.employeeSectionBtn.Click += new System.EventHandler(this.employeeSectionBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(1123, 15);
+            this.exitBtn.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(213, 86);
+            this.exitBtn.TabIndex = 5;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // dropBtn1
+            // 
+            this.dropBtn1.Location = new System.Drawing.Point(16, 144);
+            this.dropBtn1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dropBtn1.Name = "dropBtn1";
+            this.dropBtn1.Size = new System.Drawing.Size(213, 86);
+            this.dropBtn1.TabIndex = 6;
+            this.dropBtn1.Text = "New Reservation";
+            this.dropBtn1.UseVisualStyleBackColor = true;
+            this.dropBtn1.Visible = false;
+            // 
+            // dropBtn2
+            // 
+            this.dropBtn2.Location = new System.Drawing.Point(16, 238);
+            this.dropBtn2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dropBtn2.Name = "dropBtn2";
+            this.dropBtn2.Size = new System.Drawing.Size(213, 86);
+            this.dropBtn2.TabIndex = 7;
+            this.dropBtn2.Text = "Edit Reservation";
+            this.dropBtn2.UseVisualStyleBackColor = true;
+            this.dropBtn2.Visible = false;
+            // 
+            // dropBtn3
+            // 
+            this.dropBtn3.Location = new System.Drawing.Point(16, 331);
+            this.dropBtn3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dropBtn3.Name = "dropBtn3";
+            this.dropBtn3.Size = new System.Drawing.Size(213, 86);
+            this.dropBtn3.TabIndex = 8;
+            this.dropBtn3.Text = "Cancel Reservation";
+            this.dropBtn3.UseVisualStyleBackColor = true;
+            this.dropBtn3.Visible = false;
+            // 
+            // dropBtn4
+            // 
+            this.dropBtn4.Location = new System.Drawing.Point(16, 425);
+            this.dropBtn4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dropBtn4.Name = "dropBtn4";
+            this.dropBtn4.Size = new System.Drawing.Size(213, 86);
+            this.dropBtn4.TabIndex = 9;
+            this.dropBtn4.Text = "*No Use*";
+            this.dropBtn4.UseVisualStyleBackColor = true;
+            this.dropBtn4.Visible = false;
+            // 
+            // dropBtn5
+            // 
+            this.dropBtn5.Location = new System.Drawing.Point(16, 518);
+            this.dropBtn5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dropBtn5.Name = "dropBtn5";
+            this.dropBtn5.Size = new System.Drawing.Size(213, 86);
+            this.dropBtn5.TabIndex = 10;
+            this.dropBtn5.Text = "*No Use*";
+            this.dropBtn5.UseVisualStyleBackColor = true;
+            this.dropBtn5.Visible = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1357, 620);
+            this.Controls.Add(this.dropBtn5);
+            this.Controls.Add(this.dropBtn4);
+            this.Controls.Add(this.dropBtn3);
+            this.Controls.Add(this.dropBtn2);
+            this.Controls.Add(this.dropBtn1);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.employeeSectionBtn);
+            this.Controls.Add(this.clientsBtn);
+            this.Controls.Add(this.billingBtn);
+            this.Controls.Add(this.roomManagementBtn);
+            this.Controls.Add(this.reservationsBtn);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "MainForm";
+            this.Text = "Hotel Management System";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button reservationsBtn;
+        private System.Windows.Forms.Button roomManagementBtn;
+        private System.Windows.Forms.Button billingBtn;
+        private System.Windows.Forms.Button clientsBtn;
+        private System.Windows.Forms.Button employeeSectionBtn;
+        private System.Windows.Forms.Button exitBtn;
+        private System.Windows.Forms.Button dropBtn1;
+        private System.Windows.Forms.Button dropBtn2;
+        private System.Windows.Forms.Button dropBtn3;
+        private System.Windows.Forms.Button dropBtn4;
+        private System.Windows.Forms.Button dropBtn5;
+    }
+}
+
---
+++ WindowsFormsApplication1/MainForm.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+            //test
+        {
+            InitializeComponent();
+        }
+
+        int page = 0; //Use this to tell which action we want to perform on the drop buttons 
+                      //0 = Reservations ; 1 = Room Management ; 2 = Billing ; 3 = Clients ; 4 = Employee Section
+
+        private void roomManagementBtn_Click(object sender, EventArgs e)
+        {
+            page = 1;
+
+            //Change the font style
+            this.roomManagementBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.billingBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clientsBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.employeeSectionBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+
+            //Change drop buttons text
+            dropBtn1.Visible = true;
+            dropBtn2.Visible = true;
+            dropBtn3.Visible = true;
+            dropBtn4.Visible = true;
+            dropBtn5.Visible = false;
+            dropBtn1.Text = "Check Room Status";
+            dropBtn2.Text = "Clean Rooms";
+            dropBtn3.Text = "Price Rooms";
+            dropBtn4.Text = "Change Room Info";
+
+        }
+
+        private void billingBtn_Click(object sender, EventArgs e)
+        {
+            page = 2;
+
+            //Change the font style
+            this.billingBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roomManagementBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clientsBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.employeeSectionBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+
+            //Change drop buttons text
+            dropBtn1.Visible = true;
+            dropBtn2.Visible = true;
+            dropBtn3.Visible = true;
+            dropBtn4.Visible = true;
+            dropBtn5.Visible = true;
+            dropBtn1.Text = "Check out Guest";
+            dropBtn2.Text = "Guests Due Out";
+            dropBtn3.Text = "Daily Income";
+            dropBtn4.Text = "Check in Guest";
+            dropBtn5.Text = "Guests Due In";
+        }
+
+        private void clientsBtn_Click(object sender, EventArgs e)
+        {
+            page = 3;
+
+            //Change the font style
+            this.clientsBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roomManagementBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.billingBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.employeeSectionBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+
+            //Change drop buttons text
+            dropBtn1.Visible = true;
+            dropBtn2.Visible = true;
+            dropBtn3.Visible = true;
+            dropBtn4.Visible = false;
+            dropBtn5.Visible = false;
+            dropBtn1.Text = "New Client";
+            dropBtn2.Text = "Find Client";
+            dropBtn3.Text = "Edit Client Information";
+        }
+
+        private void employeeSectionBtn_Click(object sender, EventArgs e)
+        {
+            page = 4;
+
+            //Change the font style
+            this.employeeSectionBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F,
+                System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.roomManagementBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.billingBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clientsBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+
+            //Change drop buttons text
+            dropBtn1.Visible = true;
+            dropBtn2.Visible = true;
+            dropBtn3.Visible = true;
+            dropBtn4.Visible = true;
+            dropBtn5.Visible = true;
+            dropBtn1.Text = "Clock In";
+            dropBtn2.Text = "Clock Out";
+            dropBtn3.Text = "Start Meal";
+            dropBtn4.Text = "End Meal";
+            dropBtn5.Text = "Edit Employee Information";
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            this.Close(); //Close application
+        }
+
+        private void reservationsBtn_Click(object sender, EventArgs e)
+        {
+            page = 0;
+
+            //Change the font style
+            this.employeeSectionBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.roomManagementBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.billingBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clientsBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+
+            //Change drop buttons text
+            dropBtn1.Visible = true;
+            dropBtn2.Visible = true;
+            dropBtn3.Visible = true;
+            dropBtn4.Visible = false;
+            dropBtn5.Visible = false;
+            dropBtn1.Text = "New Reservation";
+            dropBtn2.Text = "Edit Reservation";
+            dropBtn3.Text = "Cancel Reservation";
+
+        }
+
+    }
+}
